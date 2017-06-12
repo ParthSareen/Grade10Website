7b6e38b90bfe507104a407e2b510e1704d0fc4c8
ParthSareen <parth0612@live.com>
2017-06-12 09:31:22 -0400

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+	$('#VacuumTubes').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px'
+		});
+	});
+	$('#VacuumTubes').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px'
+		});
+	});
+
+	$('#Transistors').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px'
+		});
+	});
+	$('#Transistors').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px'
+		});
+	});
+
+	$('#IntergratedCircuits').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px'
+		});
+	});
+	$('#IntergratedCircuits').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px'
+		});
+	});
+
+	$('#Microprocessors').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px'
+		});
+	});
+	$('#Microprocessors').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px'
+		});
+	});
+
+	$('#ArtificialIntelligence').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px'
+		});
+	});
+	$('#ArtificialIntelligence').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px'
+		});
+	});
+$('#Interviews').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px'
+		});
+	});
+$('#Interviews').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px'
+		});
+	});
+
+
+});
